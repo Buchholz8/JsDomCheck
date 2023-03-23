235f651dd32c4e1cb1dde1ca05b7c23602cc29cc
markb <markbuchholz8@gmail.com>
2023-03-23 17:45:54 -0600

    more selectors

--- js/app.js
+++ js/app.js
@@ -1,8 +1,4 @@
-let all_p = document.querySelectorAll(`p`);
-all_p
-let f_div = document.querySelector(`div`);
-f_div
+let all_p = document.querySelectorAll(`.body_p`);
+let f_div = document.querySelector(`#first_div`);
 let title_id = document.getElementById(`title_name`);
-title_id
-let footer_img = document.getElementById(`footer_pic`);
-footer_img
+let footer_img = document.getElementsByClassName(`footer_tags`);
