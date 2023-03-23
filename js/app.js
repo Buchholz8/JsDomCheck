2776058dd51be361c0792cd7715a02c5350c3ab7
markb <markbuchholz8@gmail.com>
2023-03-23 17:31:35 -0600

    using git element

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let all_p = document.querySelectorAll(`p`);
+all_p
+let f_div = document.querySelector(`div`);
+f_div
+let title_id = document.getElementById(`title_name`);
+title_id
+let footer_img = document.getElementById(`footer_pic`);
+footer_img
